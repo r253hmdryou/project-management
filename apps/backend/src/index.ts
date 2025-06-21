b66fcaaf3c2720aed505dc68d0c738718893ad30
HAMADA Ryou <r253.hmdryou@gmail.com>
2025-06-22 05:06:21 +0900

    add get projects endpoint

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from "hono/quick";
+import { PrismaClient } from "@prisma/client";
 
 const app = new Hono();
+const prisma = new PrismaClient();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.get("/projects", async (c) => {
+  const projects = await prisma.project.findMany({
+    select: {
+      uuid: true,
+      name: true,
+      instruction: true,
+    },
+  });
+  return c.json(projects);
 });
 
 app.fire();
